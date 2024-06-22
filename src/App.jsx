324cd4a6d05d0e190077a93b9f865fdfcf205689
oyinkansola <yahayaoyinkansola@gmail.com>
2024-06-22 16:26:53 +0100

    Convert Project UI to code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import "./App.css";
 
 const App = () => {
-  return <h1>Project Start</h1>;
+  return (
+    <>
+      <div className="age-container">
+        <form className="calculate-age-form">
+          <div className="input-group">
+            <label htmlFor="day">Day</label>
+            <input type="text" id="day" placeholder="DD" />
+          </div>
+
+          <div className="input-group">
+            <label htmlFor="month">Month</label>
+            <input type="text" id="day" placeholder="MM" />
+          </div>
+
+          <div className="input-group">
+            <label htmlFor="year">Year</label>
+            <input type="text" id="year" placeholder="YYYY" />
+          </div>
+        </form>
+
+        <div id="line"></div>
+
+        <div id="result">
+          <h2>
+            <span>--</span> years
+          </h2>
+
+          <h2>
+            <span>--</span> months
+          </h2>
+
+          <h2>
+            <span>--</span> days
+          </h2>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default App;
